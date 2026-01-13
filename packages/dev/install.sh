601d6240b0e09f0a800d447a7c6dd9310a30ec91
Robert Roach <rjayroach@gmail.com>
2026-01-13 08:19:32 +0800

    add bases to space

--- packages/dev/install.sh
+++ packages/dev/install.sh
@@ -1,27 +1,44 @@
 # ppm/dev
 
-# dependencies() {
-#   echo "chorus"
-# }
+dependencies() {
+  echo "chorus"
+}
 
-space_path() { hub ls --path ppm-dev; }
+space_path() { hub ls --path ppm; }
 
 space_install() {
+  install_repos
+  install_bases
+}
+
+install_repos() {
   mkdir -p repos
   pushd repos > /dev/null
 
   local repos=`hub ls --path ppm-repos`
   for repo in ppm pde-ppm pdt-ppm; do
-    [[ -L $repo ]] && continue
-    [[ ! -d $repos/$repo ]] && continue
-    ln -s $repos/$repo .
+    [[ -L "$repo" ]] && continue
+    [[ ! -d "$repos/$repo" ]] && continue
+    ln -s "$repos/$repo" .
   done
 
   local ppm_script=~/.local/bin/ppm
-  if [[ -f "$ppm_script" && ! -L "$ppm_script" ]]; then
-    rm $ppm_script
-    ln -s "$PWD/ppm/ppm" $ppm_script
-  fi
+  rm -f $ppm_script
+  ln -s "$PWD/ppm/ppm" $ppm_script
+
+  popd > /dev/null
+}
+
+install_bases() {
+  mkdir -p bases
+  pushd bases > /dev/null
+
+  local repos=`hub ls --path obsidian`
+  for repo in pde pdt; do
+    [[ -L "$repo" ]] && continue
+    [[ ! -d "$repos/$repo" ]] && continue
+    ln -s "$repos/$repo" .
+  done
 
   popd > /dev/null
 }

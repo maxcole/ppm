8ba92b3874e7104a956d7e2ddf5b18bb1a2cd8c6
Robert Roach <rjayroach@gmail.com>
2026-02-20 15:08:00 +0800

    break out chorus into a package

---
+++ packages/chorus/install.sh
@@ -0,0 +1,26 @@
+# ppm/chorus
+
+space_path() { hub ls --path ppm; }
+
+space_install() {
+  install_repos
+  install_bases
+}
+
+install_repos() {
+  repo clone ppm/user
+  local target_dir=`hub ls --path ppm-repos`
+  local repos=$(find $target_dir -maxdepth 1 -mindepth 1 -type d)
+  create_symlinks repos $repos
+}
+
+install_bases() {
+  mkdir -p bases
+  local target_dir=`hub ls --path obsidian`
+  create_symlinks bases "$target_dir/pde" "$target_dir/pdt"
+}
+
+post_install() {
+  source <(mise activate bash)
+  mise install npm:bats
+}
--- packages/dev/install.sh
+++ packages/dev/install.sh
@@ -4,34 +4,11 @@ dependencies() {
   echo "chorus"
 }
 
-pre_install() {
+# Removes the ~/.local/bin/ppm script and symlinks it to the repo
+# so changes made in the script are carried over to the repo
+post_install() {
   pushd $BIN_DIR > /dev/null
   rm ppm
   ln -s $XDG_DATA_HOME/ppm/ppm/ppm .
   popd > /dev/null
-}
-
-space_path() { hub ls --path ppm; }
-
-space_install() {
-  install_repos
-  install_bases
-}
-
-install_repos() {
-  repo clone ppm/user
-  local target_dir=`hub ls --path ppm-repos`
-  local repos=$(find $target_dir -maxdepth 1 -mindepth 1 -type d)
-  create_symlinks repos $repos
-}
-
-install_bases() {
-  mkdir -p bases
-  local target_dir=`hub ls --path obsidian`
-  create_symlinks bases "$target_dir/pde" "$target_dir/pdt"
-}
-
-post_install() {
-  source <(mise activate bash)
-  mise install npm:bats
 }

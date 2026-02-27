bf3b57ced4520d68f6fd1d8c8c593e37231aace3
Robert Roach <rjayroach@gmail.com>
2026-02-27 17:22:49 +0800

    update chorus formats

--- packages/chorus/install.sh
+++ packages/chorus/install.sh
@@ -1,6 +1,6 @@
 # ppm/chorus
 
-space_path() { hub ls --path ppm; }
+space_path() { hub_path ppm; }
 
 space_install() {
   install_repos
@@ -8,15 +8,15 @@ space_install() {
 }
 
 install_repos() {
-  repo clone ppm/user
-  local target_dir=`hub ls --path ppm-repos`
+  repo_clone ppm/user
+  local target_dir="$(hub_path ppm-repos)"
   local repos=$(find $target_dir -maxdepth 1 -mindepth 1 -type d)
   create_symlinks repos $repos
 }
 
 install_bases() {
   mkdir -p bases
-  local target_dir=`hub ls --path obsidian`
+  local target_dir="$(hub_path obsidian)"
   create_symlinks bases "$target_dir/pde" "$target_dir/pdt"
 }
 
